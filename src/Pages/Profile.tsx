327a60950e9296352c82be55a563264af6082895
Vinxkumar <vinxkumar178@gmail.com>
2026-05-06 11:57:17 +0530

    Updated Nav Bar

---
+++ src/Pages/Profile.tsx
@@ -0,0 +1,11 @@
+
+
+const Profile = () => {
+
+
+    return (
+        <>
+        
+        </>
+    );
+}
--- src/components/HomeNavBar.tsx
+++ src/components/HomeNavBar.tsx
@@ -1,13 +1,17 @@
 import React, {useState} from "react"
 import { navItems } from "../interface/navBar"
-import { CgProfile } from "react-icons/cg";
-import profilePic from "../assets/profilePic.jpg"
+import { IoSettings } from "react-icons/io5";
 import { Button } from "antd";
 import { logout } from "../utils/TokenVerifier";
 import { useNavigate } from "react-router-dom";
+import { IoMdLogOut } from "react-icons/io";
 const HomeNavBar = () => {
     const [onClickItem, setOnClickItem] = useState<number | null>(null);
     const [hover, setHover] = useState<boolean | null>(null);
+    const [buttonHover, setButtonHover] = useState<boolean>(false);
+    const [profileHover, setProfileHover] = useState<boolean>(false);
+    const [searchHover, setSearchHover] = useState<boolean>(false);
+
     const navigate = useNavigate();
     const handleOnClick = (index: number) => {
         if(onClickItem === index) {
@@ -28,7 +32,7 @@ const HomeNavBar = () => {
                             key = {index}
                             className= "relative cursor-pointer"
                         >
-                            <span onClick={() => handleOnClick(index)}>
+                            <span className="hover:text-gray-400" onClick={() => handleOnClick(index)}>
                             {item.lable}
                             </span>
                             {item.child && onClickItem === index && hover && (
@@ -44,21 +48,29 @@ const HomeNavBar = () => {
                     ))} 
                 </ul>
             </div>
-            <div className="flex items-center justify-center gap-4">
-                <div className="flex items-center gap-4">
+
+            <div className="flex items-center justify-center" onMouseEnter={() => setSearchHover(true)} onMouseLeave={() => setSearchHover(false)}>
+                <input type="text" placeholder="Search..." className={`px-4 py-2 h-8 rounded-full bg-zinc-700 text-white focus:outline-none focus:ring-1 focus:ring-blue-200  transition-all ease-in-out duration-200 ${searchHover ? "w-72" : "w-40"}`} />
+            </div>
+
+            <div className="flex items-center justify-center gap-2">
+                <div className="flex items-center px-6" onMouseEnter={() => setProfileHover(true)} onMouseLeave={() => setProfileHover(false)}>
+                    <button className={`flex items-center justify-center gap-2  font-bold ${profileHover ? "text-gray-400 cursor-pointer" : "text-white"}`} >
                     <span>{localStorage.getItem("userName")}</span>
-                    <div className="w-8 h-8 rounded-full bg-gray-500 flex items-center justify-center">
-                        <img src={profilePic} alt="Profile" className="w-full h-full object-cover rounded-full" />
-                        <CgProfile className="w-6 h-6"/>
-                    </div>
+                    </button>
                 </div>
-                <div className="flex items-center justify-center gap-4 px-6 py-[-10px] rounded-2xl">
-                    <Button type="primary" className="bg-red-400" onClick={() => {
+                <div className="flex items-center justify-center  rounded-2xl" >
+                    <button className="flex items-center justify-center gap-2  rounded-2xl transition-all duration-200  ease-in-out text-white hover:rotate-90 cursor-pointer" >
+                        <IoSettings  className="w-8 h-6"/>
+                    </button>
+                </div>
+                <div className="flex items-center justify-center gap-4 px-6 py-[-10px] rounded-2xl" onMouseEnter={() => setButtonHover(true)} onMouseLeave={() => setButtonHover(false)}>
+                    <button className={`flex items-center justify-center gap-2 px-4 py-2 rounded-2xl transition-all duration-200  ease-in-ou  text-white ${buttonHover ? "bg-red-800 cursor-pointer " : "bg-red-500 "}`} onClick={() => {
                         logout();
                         navigate("/login");
                     }}>
-                        Logout
-                    </Button>
+                        <IoMdLogOut /> 
+                    </button>
                 </div>
             </div>
         </nav>    
