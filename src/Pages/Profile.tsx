efa01461f1918951e554fd2aee72f0197e060eec
Vinxkumar <vinxkumar178@gmail.com>
2026-05-07 14:58:29 +0530

    Added Profile component in profile page

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,11 +1,15 @@
+import Profile from "../components/ProfileCard";
 
-
-const Profile = () => {
+const ProfilePage = () => {
 
 
     return (
         <>
-        
+        <div className="flex w-full bg-zinc-700  h-screen justify-center items-center">
+            <Profile />
+        </div>
         </>
     );
 }
+
+export default ProfilePage;
