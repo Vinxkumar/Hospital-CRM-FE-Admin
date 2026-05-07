e3cb336410c453be24a8eaa4dea8227ab9b475d2
Vinxkumar <vinxkumar178@gmail.com>
2026-05-07 14:55:48 +0530

    Updated Home Screen

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,26 +1,21 @@
 import { useEffect, useRef } from "react"
 import HomeNavBar from "../components/HomeNavBar"
 import { message } from 'antd'
-
+import KeyMetrics from "../components/KeyMetrics"
 const HomePage = () => {
-    const [messageApi, contextHolder] = message.useMessage();
-    const hasShown = useRef(false);
-    useEffect(() => {
-        if(hasShown.current) return;
-        messageApi.open({
-            type: 'success',
-            content: `Welcome ${localStorage.getItem("userName")} !`
-        });
-        hasShown.current = true;
-    }, [messageApi]);
 
     return (
         <>
-            {contextHolder}
-            <div className="bg-zinc-700 h-screen">
+
+            <div className="bg-zinc-700 h-screen w-screen flex flex-col gap-8 items-center justify-start">
                 <div className="flex item-center justify-center">
                     <HomeNavBar />
                 </div>
+                <div className="flex gap-8 justify-between items-start w-17/18 px-8">
+                    <KeyMetrics />
+
+                </div>
+                
             </div>
         </>
     )
