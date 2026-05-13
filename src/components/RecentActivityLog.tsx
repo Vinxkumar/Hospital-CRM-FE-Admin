631746b31ce090719ffbcbc2a1e9e21192f5f41e
Vinxkumar <vinxkumar178@gmail.com>
2026-05-13 18:46:08 +0530

    updated Recent activity log

--- src/components/RecentActivityLog.tsx
+++ src/components/RecentActivityLog.tsx
@@ -9,11 +9,11 @@ const RecentActivityLog = () => {
 <motion.div
     initial={{ opacity: 0, scale: 0.5, x: 100 }}
     animate={{ opacity: 1, scale: 1, x: 0 }}
-    whileHover={{ scale: 1.01, boxShadow: "0px 0px 20px rgba(255, 255, 255, 0.5)" }}
+    whileHover={{ scale: 1.01, boxShadow: "0 0 20px rgba(72, 187, 120, 0.5)" }}
     transition={{
-        default: { duration: 0.5, delay: 0.2 },  // for initial → animate
-        scale: { duration: 0.2 },                  // for hover scale
-        boxShadow: { duration: 0.2 }               // for hover shadow
+        default: { duration: 0.5, delay: 0.2 },  
+        scale: { duration: 0.2 },                
+        boxShadow: { duration: 0.2 }             
     }}
     className="flex flex-col gap-4 p-4 bg-zinc-800 rounded-2xl w-full h-full"
 >
