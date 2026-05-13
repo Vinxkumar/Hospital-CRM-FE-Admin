6ba86fcb53d13dd776ea3d7abad9f6041a0a3a5d
Vinxkumar <vinxkumar178@gmail.com>
2026-05-13 18:45:31 +0530

    Updated Hover Color to Green

--- src/components/KeyMetrics.tsx
+++ src/components/KeyMetrics.tsx
@@ -1,4 +1,4 @@
-import {motion, scale} from "motion/react"
+import {motion} from "motion/react"
 import KeyMetricsHandler from "../handler/KeyMetricsHandler";
 import { FaUserDoctor } from "react-icons/fa6";
 import { CiMedicalMask } from "react-icons/ci";
@@ -9,11 +9,11 @@ const keyMetrics = () => {
     <motion.div
       initial={{ opacity: 0, scale: 0.5, x: -100 }}
       animate={{ opacity: 1, scale: 1, x: 0 }}
-      whileHover={{scale:1.01, boxShadow: "0px 0px 20px rgba(255, 255, 255, 0.5)" }}
+      whileHover={{scale:1.01, boxShadow: "0 0 20px rgba(72, 187, 120, 0.5)" }}
           transition={{
-        default: { duration: 0.5, delay: 0.1 },  // for initial → animate
-        scale: { duration: 0.2 },                  // for hover scale
-        boxShadow: { duration: 0.2 }               // for hover shadow
+        default: { duration: 0.5, delay: 0.1 },  
+        scale: { duration: 0.2 },                  
+        boxShadow: { duration: 0.2 }               
     }}
       className="flex flex-col gap-4 p-4 bg-zinc-800 rounded-2xl w-full h-54 "
     >
@@ -25,7 +25,7 @@ const keyMetrics = () => {
 
                     <div className="flex gap-4">
 
-                        <button className="flex items-center gap-4 px-4 py-2 text-white bg-zinc-700 rounded-xl transition-all duration-200 ease-in-out hover:bg-zinc-600">
+                        <button className="flex items-center gap-4 px-4 py-2 text-white bg-green-500 rounded-xl transition-all duration-100 ease-in-out hover:bg-green-600">
                             <CiMedicalMask className="w-12 h-12 text-white"/>
                         </button>
 
@@ -41,7 +41,7 @@ const keyMetrics = () => {
                 <div className="flex gap-4 flex-col space-x-8">
                     <div className="flex gap-4">
 
-                        <button className="flex items-center gap-4 px-4 py-2 text-white bg-zinc-700 rounded-xl transition-all duration-200 ease-in-out hover:bg-zinc-600">
+                        <button className="flex items-center gap-4 px-4 py-2 text-white bg-green-500 rounded-xl transition-all duration-200 ease-in-out hover:bg-green-600">
                             <FaUserDoctor className="w-12 h-12 text-white"/>
                         </button>
 
@@ -56,7 +56,7 @@ const keyMetrics = () => {
                 <div className="flex gap-4 flex-col ">
                     <div className="flex gap-4">
 
-                        <button className="flex items-center gap-4 px-4 py-2 text-white bg-zinc-700 rounded-xl transition-all duration-200 ease-in-out hover:bg-zinc-600">
+                        <button className="flex items-center gap-4 px-4 py-2 text-white bg-green-500 rounded-xl transition-all duration-200 ease-in-out hover:bg-green-600">
                             <FaSuitcaseMedical className="w-12 h-12 text-white"/>
                         </button>
 
