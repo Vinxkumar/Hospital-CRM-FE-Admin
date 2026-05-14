393bbb8e7508dd559b439c31df4cc3a92ff60d3b
Vinxkumar <vinxkumar178@gmail.com>
2026-05-14 22:44:50 +0530

    Changed the theme

--- src/components/HomeNavBar.tsx
+++ src/components/HomeNavBar.tsx
@@ -89,32 +89,73 @@ const HomeNavBar = ({ scrollY }: ScrollPosition) => {
         </ul>
       </div>
 
-            <div className="flex items-center justify-center" onMouseEnter={() => setSearchHover(true)} onMouseLeave={() => setSearchHover(false)}>
-                <input type="text" placeholder="Search..." className={`px-4 py-2 h-8 rounded-full bg-zinc-700 text-white focus:outline-none focus:ring-1 focus:ring-blue-200  transition-all ease-in-out duration-200 ${searchHover ? "w-72" : "w-40"}`} />
-            </div>
+      <div
+        className="flex items-center justify-center"
+        onMouseEnter={() => setSearchHover(true)}
+        onMouseLeave={() => setSearchHover(false)}
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          className={`px-4 py-2 h-8 rounded-full bg-slate-700 border-2 border-slate-600 text-white focus:outline-none focus:ring-1 focus:ring-blue-200  transition-all ease-in-out duration-200 ${searchHover ? "w-72" : "w-40"}`}
+        />
+      </div>
 
-            <div className="flex items-center justify-center gap-2">
-                <div className="flex items-center px-6" onMouseEnter={() => setProfileHover(true)} onMouseLeave={() => setProfileHover(false)}>
-                    <button className={`flex items-center justify-center gap-2  font-bold ${profileHover ? "text-gray-400 cursor-pointer" : "text-white"}`} onClick={()=> navigate("/profile")}>
-                    <span>{localStorage.getItem("userName")}</span>
-                    </button>
-                </div>
-                <div className="flex items-center justify-center  rounded-2xl" >
-                    <button className="flex items-center justify-center gap-2  rounded-2xl transition-all duration-200  ease-in-out text-white hover:rotate-90 cursor-pointer" >
-                        <IoSettings  className="w-8 h-6"/>
-                    </button>
-                </div>
-                <div className="flex items-center justify-center gap-4 px-6 py-[-10px] rounded-2xl" onMouseEnter={() => setButtonHover(true)} onMouseLeave={() => setButtonHover(false)}>
-                    <button className={`flex items-center justify-center gap-2 px-4 py-2 rounded-2xl transition-all duration-200  ease-in-ou  text-white ${buttonHover ? "bg-red-800 cursor-pointer " : "bg-red-500 "}`} onClick={() => {
-                        logout();
-                        navigate("/login");
-                    }}>
-                        <IoMdLogOut /> 
-                    </button>
-                </div>
-            </div>
-        </nav>    
-    )
+      <div className="flex items-center justify-center gap-2">
+        <div
+          className="flex items-center px-6"
+          onMouseEnter={() => setProfileHover(true)}
+          onMouseLeave={() => setProfileHover(false)}
+        >
+          <motion.button
+            className={`relative flex items-center justify-center gap-2  font-bold ${profileHover ? "text-gray-400 cursor-pointer" : "text-white"}`}
+            onClick={() => navigate("/profile")}
+            initial="rest"
+            whileHover="hover"
+          >
+            <span>{localStorage.getItem("userName")}</span>
+                          <motion.div
+                className="absolute -bottom-1 left-0 h-0.5 w-full origin-left rounded-full bg-white"
+                variants={{
+                  rest: {
+                    scaleX: 0,
+                  },
+                  hover: {
+                    scaleX: 1,
+                  },
+                }}
+                transition={{
+                  duration: 0.2,
+                  ease: "easeInOut",
+                }}
+              />
+          </motion.button>
+        </div>
+        <div className="flex items-center justify-center  rounded-2xl">
+          <button className="flex items-center justify-center gap-2  rounded-2xl transition-all duration-200  ease-in-out text-white hover:rotate-90 cursor-pointer">
+            <IoSettings className="w-8 h-6" />
+          </button>
+        </div>
+        <div
+          className="flex items-center justify-center gap-4 px-6 py-2.5 rounded-2xl"
+          onMouseEnter={() => setButtonHover(true)}
+          onMouseLeave={() => setButtonHover(false)}
+        >
+          <button
+            className={`flex items-center justify-center gap-2 px-4 py-2 rounded-2xl transition-all duration-200  ease-in-ou  text-white ${buttonHover ? "bg-red-800 cursor-pointer " : "bg-red-500 "}`}
+            onClick={() => {
+              logout();
+              navigate("/login");
+            }}
+          >
+            <IoMdLogOut />
+          </button>
+        </div>
+      </div>
+    </motion.nav>
+  );
+};
+export default HomeNavBar;
 
 }
 export default HomeNavBar
